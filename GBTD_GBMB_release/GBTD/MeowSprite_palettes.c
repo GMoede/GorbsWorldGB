ff50963cec81056bf930a1326d93f7c53da257c6
GMoede <griffinmoede@gmail.com>
2025-11-23 15:37:26 -0800

    crazy movement

---
+++ GBTD_GBMB_release/GBTD/MeowSprite_palettes.c
@@ -0,0 +1,7 @@
+#include <gb/gb.h>
+
+/* Definitions for Super Gameboy palettes for MeowSprite */
+const UWORD MeowSpriteSGBPal0[4] = {6076, 8935, 6596, 5344};
+const UWORD MeowSpriteSGBPal1[4] = {6076, 8935, 6596, 5344};
+const UWORD MeowSpriteSGBPal2[4] = {6076, 8935, 6596, 5344};
+const UWORD MeowSpriteSGBPal3[4] = {6076, 8935, 6596, 5344};
