d47c5d4dd419bebfec1a92caef2cf086e9c81076
GMoede <griffinmoede@gmail.com>
2025-12-01 17:48:09 -0800

    adding tutorial files and gorby sleeping sprite

--- gbdk-tilemap-collision-master/src/common.c
+++ gbdk-tilemap-collision-master/src/common.c
@@ -1,8 +1,7 @@
 #include <gb/gb.h>
 #include <gb/cgb.h>
-#include <gb/metasprites.h>
 #include <stdint.h>
-#include "common.h"
+#include <gb/metasprites.h>
 #include "tileset.h"
 #include "tilemap.h"
 #include "ball.h"
@@ -89,35 +88,4 @@ void SetupDemo(){
     playerX=80;
     playerY=90;
 
-}
-
-// This is an easy way to determine a direction for a object with two frames per direction
-const Vector8 directionsForTwoFrameObjects[7]={
-    {0,1}, // Down
-    {0,0}, 
-    {0,-1}, // Up,
-    {0,0},
-    {1,0},// Right
-    {0,0},
-    {-1,0},// Left
-};
-
-uint8_t joypadCurrent=0;
-uint8_t twoFrameCounter= 0;
-uint8_t twoFrameRealValue=0;
-
-
-/**
- * @brief Our moblin and link has two frame walk animation. They'll share a common variable that determines which frame to show
- * when they are walking. 
- */
-void updateTwoFrameCounter(void){
-    twoFrameCounter+=3;
-    twoFrameRealValue = twoFrameCounter>>4;
-
-    // Stop & reset if the vlaue is over 2
-    if(twoFrameRealValue>=10){
-        twoFrameRealValue=0;
-        twoFrameCounter=0;
-    }
 }
